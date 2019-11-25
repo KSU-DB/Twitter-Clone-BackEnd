dcb3a07f3784e2be19abddfd2d5e6027d0b208e3
mkshin96 <tlsanrhs96@gmail.com>
2019-11-25 17:51:41 +0900

    create project, update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Twitter-Clone-BackEnd
+
+[도메인 및 API](https://www.notion.so/ks14/index-fda26bc8b43442ba94772eb2b19f32c8)
---
+++ twitterclone/src/test/java/me/dblab/twitterclone/TwittercloneApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dblab.twitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwittercloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
